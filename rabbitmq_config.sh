165ab175266da8993d59c2632824fe555f98957d
Cleiton Dantas <cleitonpanao@gmail.com>
2024-06-07 18:39:48 -0300

    Modificado servico de mensageria

--- rabbitmq_config.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Criar exchanges
-rabbitmqctl exchange declare -t topic pagamento-request-exchange
-# Criar queues
-rabbitmqctl queue declare -n pagamento-request-queue -d true
-# Criar bindings
-rabbitmqctl queue bind pagamento-request-queue pagamento-request-exchange pagamento-request-rout-key
